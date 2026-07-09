870dd961fe86551f2368444e052c892cf1ff4d7f
HyunSoo Park <104089770+ChordCon@users.noreply.github.com>
2026-07-09 15:12:47 +0900

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // 여기서 감싸기
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/Noctis">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
